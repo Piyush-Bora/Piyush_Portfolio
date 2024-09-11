082e3b463080dd09e77a891e707f6b6e91d33922
Piyush Bora <85740745+Piyush-Bora@users.noreply.github.com>
2024-09-12 00:25:46 +0530

    updated contents for skills and projecs and refactoring code

--- src/Content.js
+++ src/Content.js
@@ -79,34 +79,109 @@ export const content = {
     subtitle: "MY TOP SKILLS",
     skills_content: [
       {
+        name: "HTML",
+        para: "Markup language for web structure.",
+        logo: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
+      },
+      {
+        name: "CSS",
+        para: "Stylesheets for web presentation.",
+        logo: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+      },
+      {
+        name: "Tailwind CSS",
+        para: "Utility-first CSS framework.",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1200px-Tailwind_CSS_Logo.svg.png",
+      },
+      {
+        name: "JavaScript",
+        para: "Interactive web programming language.",
+        logo: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+      },
+      {
+        name: "ReactJS",
+        para: "Library for building UIs.",
+        logo: "https://cdn-icons-png.flaticon.com/512/760/760457.png",
+      },
+      {
+        name: "AngularJS",
+        para: "Framework for dynamic web apps.",
+        logo: "https://th.bing.com/th/id/OIP.FFvMsDBfJpitfMsfaiuQwgHaFj?w=251&h=188&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+      },
+      // {
+      //   name: "Python",
+      //   para: "Versatile high-level language.",
+      //   logo: "https://cdn-icons-png.flaticon.com/512/5968/5968350.png",
+      // },
+      {
+        name: "C/C++",
+        para: "Powerful, efficient programming languages.",
+        logo: "https://th.bing.com/th/id/OIP.FIOlwRb1ITHhWbOGxVgV4gHaHa?w=212&h=212&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+      },
+      {
+        name: "MySQL",
+        para: "Popular relational database system.",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlygBZ1TrcGKZlQgk7FW8XfqwMB5HGXJr8AidPMuglLe8z8Z1WHK_aSj0AfJTHDB9J2aw&usqp=CAU",
+      },
+      {
         name: "Figma",
-        para: "Lorem ipsum text  dummy",
-        logo: figma,
+        para: "Design tool for interfaces.",
+        logo: "https://cdn-icons-png.flaticon.com/512/5968/5968705.png",
       },
       {
-        name: "Node js",
-        para: "Lorem ipsum text  dummy",
-        logo: nodejs,
+        name: "Photoshop",
+        para: "Image editing and design.",
+        logo: "https://cdn-icons-png.flaticon.com/512/5968/5968520.png",
+      },
+      // {
+      //   name: "InDesign",
+      //   para: "Desktop publishing and layout.",
+      //   logo: "https://cdn-icons-png.flaticon.com/512/5968/5968482.png",
+      // },
+      // {
+      //   name: "Premiere Pro",
+      //   para: "Video editing software.",
+      //   logo: "https://cdn-icons-png.flaticon.com/512/5968/5968525.png",
+      // },
+      // {
+      //   name: "After Effects",
+      //   para: "Motion graphics and effects.",
+      //   logo: "https://cdn-icons-png.flaticon.com/512/5968/5968428.png",
+      // },
+      {
+        name: "Canva",
+        para: "Graphic design made easy.",
+        logo: "https://www.freelogovectors.net/wp-content/uploads/2021/12/canva-logo-app-freelogovectors.net_.png",
       },
       {
-        name: "Adobe Photoshop",
-        para: "Lorem ipsum text  dummy",
-        logo: ps,
+        name: "GitHub",
+        para: "Code repository and versioning.",
+        logo: "https://cdn-icons-png.flaticon.com/512/733/733609.png",
+      },
+      // {
+      //   name: "OBS",
+      //   para: "Software for streaming and recording.",
+      //   logo: "https://banner2.cleanpng.com/20180411/qve/kisspng-open-broadcaster-software-computer-software-comput-studio-5ace5f9f9c5f33.2137552515234743356405.jpg",
+      // },
+      {
+        name: "C# Dotnet",
+        para: "Microsoft's programming framework.",
+        logo: "https://th.bing.com/th/id/OIP.BuejBDkS51ZUvyqh7JKaCQHaHa?w=171&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7",
       },
       {
-        name: "React js",
-        para: "Lorem ipsum text  dummy",
-        logo: reactjs,
+        name: "Java",
+        para: "Versatile object-oriented language.",
+        logo: "http://ts2.mm.bing.net/th?id=OIP.eyVWUDy9kDXVGDGv1Ev4iAAAAA&pid=15.1",
       },
       {
-        name: "Sketch",
-        para: "Lorem ipsum text  dummy",
-        logo: sketch,
+        name: "AWS",
+        para: "Cloud computing services platform.",
+        logo: "https://th.bing.com/th/id/OIP.Sd4Hxz8_CqID29kyj5-k9wHaEc?w=310&h=186&c=7&r=0&o=5&dpr=1.1&pid=1.7",
       },
       {
-        name: "Python",
-        para: "Lorem ipsum text  dummy",
-        logo: python,
+        name: "SQL Server",
+        para: "Microsoft's relational database management.",
+        logo: "https://th.bing.com/th/id/OIP.sluuRP9RbH3MPqzbFNLEmQHaF_?w=215&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7",
       },
     ],
     icon: MdArrowForward,
@@ -117,12 +192,12 @@ export const content = {
     service_content: [
       {
         title: "Web Development",
-        para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        para: "Building modern, responsive websites using cutting-edge technologies for an optimal user experience.",
         logo: services_logo1,
       },
       {
-        title: "ui / ux DESIGNING",
-        para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+        title: "UI / UX Designing",
+        para: "Creating intuitive and engaging designs that enhance user experience and drive meaningful interactions.",
         logo: services_logo2,
       },
       // {
@@ -138,16 +213,54 @@ export const content = {
     image: person_project,
     project_content: [
       {
-        title: "Gym Website",
-        image: project1,
+        id: "p1",
+        title: "Daily Wage Workers",
+        description:
+          "Developed a website for easing the job finding for daily wage workers. The recruiters post their job requirements and the worker then chooses the job according to his preferences.",
+        tech: ["Tailwind-CSS", "Django"],
+        img: "https://media.istockphoto.com/vectors/construction-staff-characters-design-include-foreman-painter-team-vector-id1081790264?k=20&m=1081790264&s=612x612&w=0&h=TIN4U37wmzbyFgsN8ty3kh3h9oMtzGuDuO4ZhoIzbeY=",
+        github: "https://github.com/deveshbedmutha1406/final_pbl",
+        hostURL: "/",
       },
       {
-        title: "Social Media web",
-        image: project2,
+        id: "p2",
+        title: "News Reading Website",
+        description:
+          "Made a news Reading Website for daily news updates. This app fetches data from an API category-wise and displays it in the form of cards to the user which are redirected to the article.",
+        tech: ["Tailwind-CSS", "ReactJS", "Fetch-API"],
+        img: "https://news.mit.edu/sites/default/files/images/202109/MIT-Facebook-Facts-01-press.jpg",
+        github: "https://github.com/Piyush-Bora/NewsMonkey",
+        hostURL: "/",
       },
       {
-        title: "Creative Website",
-        image: project3,
+        id: "p3",
+        title: "Jersey Store",
+        description:
+          "Developed a website for easing the job finding for daily wage workers. The recruiters post their job requirements and the worker then chooses the job according to his preferences.",
+        tech: ["Tailwind-CSS", "ReactJS"],
+        img: "https://cdn.dribbble.com/users/2414448/screenshots/16916963/media/089dd75237ef3a9f4ea07241fbb3dd11.png?compress=1&resize=400x300",
+        github: "https://github.com/Piyush-Bora/Jersey_Store",
+        hostURL: "https://jerseystore.netlify.app/",
+      },
+      {
+        id: "p4",
+        title: "WhatsApp Pinger",
+        description:
+          "Developed a website for easing the task of messaging someone on WhatsApp without ssaving their phone number. User has to enter the phone Number and that contact will open in whatsApp.",
+        tech: ["Tailwind-CSS", "ReactJS"],
+        img: "https://cdn.dribbble.com/userupload/3020703/file/original-80ecfa10e4fbbb510183df9a9e75d0a2.png?resize=400x0",
+        github: "/",
+        hostURL: "https://simplewa.netlify.app/",
+      },
+      {
+        id: "p5",
+        title: "Technopedia",
+        description:
+          "Developed a website for a hackathon in PCCOE College. This website gives a introduction of the technical world, courses and roadmaps for different technologies.",
+        tech: ["Tailwind-CSS", "ReactJS"],
+        img: "https://pcprd.azureedge.net/content/c541aa03f724.jpg",
+        github: "/",
+        hostURL: "https://technopedia-piyush-bora.netlify.app/",
       },
     ],
   },
@@ -190,7 +303,7 @@ export const content = {
     btnText: "Hire Me",
   },
   Contact: {
-    title: "Contect Me",
+    title: "Contact Me",
     subtitle: "GET IN TOUCH",
     social_media: [
       {
@@ -212,5 +325,52 @@ export const content = {
   },
   Footer: {
     text: "All © Copy Right Reserved 2022",
+  },
+  WorkEx: {
+    title: "Work Experience",
+    subtitle: "PLACES I WORKED AT",
+    experiences: [
+      {
+        company: "Amber Student",
+        date: "May 2023 - Jan 2024",
+        role: "FrontEnd Developer Intern",
+        achievements: [
+          "Developed API to retrieve user location data, boosting lead generation by 5% and facilitating traffic analysis.",
+          "Revamped websites in Webflow for a cleaner UI, positively impacting user experience.",
+          "Enhanced tracking capabilities by adding GTM events for better analytics.",
+          "Integrated MoEngage SDK for web push notifications, increasing user engagement and conversion.",
+          "Improved website SEO by 22% using anchor-based titles and paginations.",
+          "Collaborated effectively in an agile environment, delivering high-quality solutions and fostering a positive team atmosphere.",
+        ],
+        skills: ["UI/UX", "Photoshop", "Illustrator", "Figma", "Canva"],
+        link: "https://amberstudent.com/",
+      },
+      {
+        company: "Sanspots",
+        date: "Jan 2023 - Apr 2023",
+        role: "UI/UX Designer Intern",
+        achievements: [
+          "Designed user-centric interfaces for web and mobile applications.",
+          "Conducted user research and usability testing to inform design decisions.",
+          "Collaborated with cross-functional teams to ensure alignment throughout the design and development process.",
+        ],
+        skills: ["UI/UX", "Photoshop", "Illustrator", "Figma", "Canva"],
+        link: "https://www.wevibrancy.com/",
+      },
+      // Add more work experiences here following the same structure
+      {
+        company: "AutomatonAI",
+        date: "Oct 2022 - Dec 2022",
+        role: "Frontend Web Developer Intern",
+        achievements: [
+          "Worked on new website for the AdaptAI product using MERN stack.",
+          "Learnt the basics of Angular and implemented knowledge in existing websites.",
+          "Gained experience working in a team, improving communication and collaboration skills.",
+        ],
+        skills: ["HTML", "CSS", "ReactJS", "Rest API"],
+        link: "https://www.wevibrancy.com/",
+      },
+      // { ... (Optional: Add your graphic design internship details here) }
+    ],
   },
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,44 +11,46 @@ import { Pagination } from "swiper";
 const Projects = () => {
   const { Projects } = content;
   return (
-    <section className="bg-bg_light_primary" id="projects">
-      <div className="md:container px-5 pt-14 flex flex-col justify-between">
+    <section className='bg-bg_light_primary' id='projects'>
+      <div className='md:container px-5 pt-14 flex flex-col justify-between'>
         <div>
-          <h2 className="title" data-aos="fade-down">
+          <h2 className='title' data-aos='fade-down'>
             {Projects.title}
           </h2>
-          <h4 className="subtitle" data-aos="fade-down">
+          <h4 className='subtitle' data-aos='fade-down'>
             {Projects.subtitle}
           </h4>
           <br />
         </div>
-        <div className="flex items-center lg:flex-row flex-col-reverse gap-5">
+        <div className='flex items-center lg:flex-row flex-col-reverse gap-5'>
           <img
             src={Projects.image}
-            alt="..."
-            data-aos="fade-right"
-            className="max-w-[45vw] min-w-[22rem]"
+            alt='...'
+            data-aos='fade-right'
+            className='max-w-[45vw] min-w-[22rem]'
           />
           <Swiper
             pagination={{
               clickable: true,
             }}
-            data-aos="fade-left"
+            data-aos='fade-left'
             spaceBetween={20}
             modules={[Pagination]}
-            className="rounded-3xl pb-16 max-w-xs drop-shadow-primary self-start"
+            className='rounded-3xl pb-16 max-w-xs drop-shadow-primary self-start'
           >
             {Projects.project_content.map((content, i) => (
               <SwiperSlide
                 key={i}
-                className="bg-white rounded-3xl p-5 border-b-8 border-[#FAF9FD] h-fit"
+                className='bg-white rounded-3xl p-5 border-b-8 border-[#FAF9FD] h-fit min-h-full'
               >
-                <img src={content.image} alt="..." />
-                <div className="flex flex-col gap-1 mt-2">
-                  <h5 className="font-bold font-Poppins">{content.title}</h5>
-                  <button className="font-bold text-gray self-end">
+                <img src={content.img} alt='...' />
+                <div className='flex flex-col gap-1 mt-2'>
+                  <h5 className='font-bold font-Poppins'>{content.title}</h5>
+                  {/* <button className="font-bold text-gray self-end">
                     READ MORE
-                  </button>
+                  </button> */}
+
+                  <p>{content.description}</p>
                 </div>
               </SwiperSlide>
             ))}
